b40fef40e958d0e940b6f6137b889a84b42b2c74
Darren Bruning <darren@bruning.net.nz>
2024-09-03 11:48:44 +1200

    Repro as console and wpf projects

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Diagnostics.NETCore.Client;
+using Path = System.IO.Path;
+
+namespace WpfApp1;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        var dumpType = DumpType.Normal;
+// var dumpType = DumpType.WithHeap;
+        var diagnosticsClient = new DiagnosticsClient(Process.GetCurrentProcess().Id);
+        var timestamp = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss");
+        var minidumpFilePath = Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) , $"{dumpType}_wpf_dump_{timestamp}.dmp");
+        diagnosticsClient.WriteDump(dumpType, minidumpFilePath, true);
+    }
+}
---
+++ console/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+using Microsoft.Diagnostics.NETCore.Client;
+
+Console.WriteLine("Going to write dump");
+// Console.ReadLine();
+
+var dumpType = DumpType.Normal;
+// var dumpType = DumpType.WithHeap;
+var diagnosticsClient = new DiagnosticsClient(Process.GetCurrentProcess().Id);
+var timestamp = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss");
+var minidumpFilePath = Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) , $"{dumpType}_console_dump_{timestamp}.dmp");
+diagnosticsClient.WriteDump(dumpType, minidumpFilePath, true);
+
+// Console.WriteLine("After writeDump, press any key to exit");
+// var _= Console.ReadKey();
